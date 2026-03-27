e48530888873c93138b4d5ed2125e375ba304615
enicollart <enicollart@gmail.com>
2026-03-27 16:43:38 -0400

    update

--- flower_r124_brightness.js
+++ flower_r124_brightness.js
@@ -587,7 +587,7 @@ const BASE_SPEED  = _speedMode === 'crawl' ? 0.0005 : _speedMode === 'run' ? 0.0
   noiseCanvas.style.pointerEvents  = 'none';
   noiseCanvas.style.zIndex         = '999';
   noiseCanvas.style.opacity        = '0.18';
-  noiseCanvas.style.mixBlendMode   = 'difference';
+  noiseCanvas.style.mixBlendMode   = 'screen';
   document.body.appendChild(noiseCanvas);
   const ctx = noiseCanvas.getContext('2d');
   const w = window.innerWidth;
